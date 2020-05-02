b2604a55376abc383e98e7ed98fd3099c78a309b
Rinaroar <marina.luxin@gmail.com>
2020-05-02 19:03:28 +0200

    add feature

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react'
 
+
 const AddUserForm = props => {
+  const initialFormState = { id: null, name: '', username: '' }
+  const [user, setUser] = useState(initialFormState)
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+
+    setUser({ ...user, [name]: value })
+  }
+
   return (
-    <form>
-      <label>What ?</label>
-      <input type="text" name="name" value="" />
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+        if (!user.name || !user.username) return
 
-      <label>Write more</label>
-      <input type="text" name="username" value="" />
+        props.addUser(user)
+        setUser(initialFormState)
+      }}
+    >
+      <label>Name</label>
+      <input type="text" name="name" value={user.name} onChange={handleInputChange} />
 
+      <label>Username</label>
+      <input type="text" name="username" value={user.username} onChange={handleInputChange} />
       <button>Add new user</button>
+
     </form>
   )
 }
